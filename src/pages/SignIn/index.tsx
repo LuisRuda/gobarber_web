00cccd01ae2498af7301752144e6f94a749a4dde
Luis Paulo Ruda <luispauloruda@gmail.com>
2020-11-11 21:25:35 -0300

    Página de login

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
+import { Container, Content, Background } from './styles';
+
+import logoImg from '../../assets/images/logo.svg';
 
 const SignIn: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="GoBarber" title="GoBarber" />
+
+        <form>
+          <h1>Faça seu logon</h1>
+
+          <input type="text" placeholder="E-mail" />
+          <input type="passsword" placeholder="Senha" />
+          <button type="submit">Entrar</button>
+
+          <a href="forgot">Esqueci minha senha</a>
+        </form>
+
+        <a href="login">
+          <FiLogIn />
+          Criar conta
+        </a>
+      </Content>
+
+      <Background />
+    </Container>
+  );
 };
 
 export default SignIn;
