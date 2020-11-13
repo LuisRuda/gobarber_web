f92af8f4a3bbf432a3adff32a6cedbb524886b97
Luis Paulo Ruda <luispauloruda@gmail.com>
2020-11-12 23:25:23 -0300

    Login pelo Contexto

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,10 @@
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useRef, useContext } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 
+import { AuthContext } from '../../context/AuthContext';
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/images/logo.svg';
@@ -13,11 +14,18 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
+  const { signIn } = useContext(AuthContext);
+
   const handleSubmit = useCallback(
-    async (data: { email: string; password: string }) => {
+    async (data: SignInFormData) => {
       try {
         formRef.current?.setErrors({});
 
@@ -31,14 +39,15 @@ const SignIn: React.FC = () => {
         await schema.validate(data, {
           abortEarly: false,
         });
-        console.log(data);
+
+        signIn(data);
       } catch (err) {
         const errors = getValidationErrors(err);
 
         formRef.current?.setErrors(errors);
       }
     },
-    []
+    [signIn]
   );
 
   return (
